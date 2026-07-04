687a0c413b676125984f5e9a1f8da0d1f72463c1
Donghoon Seo <naosdh@naver.com>
2026-07-04 16:29:29 +0900

    chore: 백엔드 스프링 부트 초기 환경 세팅

---
+++ src/main/java/com/tryna/TrynaApplication.java
@@ -0,0 +1,13 @@
+package com.tryna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrynaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrynaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tryna/TrynaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tryna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrynaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
