b9374bf48cb39b15ad4927f4e73ce2d395ff62b3
taerimiiii <shipton0201@gmail.com>
2026-07-05 21:41:41 +0900

    feat: Users와 UserSettings 1:1 양방향 매핑 추가

--- src/main/java/com/tryna/domain/user/entity/Users.java
+++ src/main/java/com/tryna/domain/user/entity/Users.java
@@ -6,9 +6,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
@@ -45,5 +47,8 @@ public class Users extends BaseEntity {
 
     @Column(name = "deleted_at")
     private LocalDateTime deletedAt;
+
+    @OneToOne(mappedBy = "user", fetch = FetchType.LAZY)
+    private UserSettings userSettings;
 
 }
