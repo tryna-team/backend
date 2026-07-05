b19b0fea81bd0327c8f70d0cbaee379742af9810
Donghoon Seo <naosdh@naver.com>
2026-07-06 01:55:49 +0900

    chore: Swagger 보안 표기 컨벤션 적용 및 application-prod.yaml 추가

--- src/main/java/com/tryna/global/config/OpenApiConfig.java
+++ src/main/java/com/tryna/global/config/OpenApiConfig.java
@@ -3,7 +3,6 @@ package com.tryna.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
@@ -25,8 +24,7 @@ public class OpenApiConfig {
                         .addSecuritySchemes(BEARER_SECURITY_SCHEME, new SecurityScheme()
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
-                                .bearerFormat("JWT")))
-                .addSecurityItem(new SecurityRequirement().addList(BEARER_SECURITY_SCHEME));
+                                .bearerFormat("JWT")));
     }
 
     @Bean
