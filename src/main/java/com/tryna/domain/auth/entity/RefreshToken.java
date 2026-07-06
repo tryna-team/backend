91ccb8b70aa4f020105691e49350cad6e7c44741
taerimiiii <shipton0201@gmail.com>
2026-07-06 20:05:00 +0900

    style: 주석 추가

--- src/main/java/com/tryna/domain/auth/entity/RefreshToken.java
+++ src/main/java/com/tryna/domain/auth/entity/RefreshToken.java
@@ -7,7 +7,8 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 
-// CHECK_TOKEN: redis 테이블임을 명시하기 위해 refresh_token -> token_redis로 변경
+// CHECK_TOKEN: redis 테이블임을 명시하기 위해 refresh_token -> token_redis로 변경. 또한 fcm_token 역시 해당 테이블에 저장될 예정
+// 테이블 명으로 유지할지, refresh_token 으로 변경할지 확인 필요.
 @RedisHash("token_redis")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -20,6 +21,8 @@ public class RefreshToken {
 
     @TimeToLive
     private Long expiration;
+
+    // TODO_FCM: fcm_token 추가
 
     public static RefreshToken create(Long userId, String provider, String token, long ttlSeconds) {
         RefreshToken refreshToken = new RefreshToken();
