f82ee2bc1e827652dfa660ab90b7a4f5429f07f6
Ubokchi <105459234+Ubokchi@users.noreply.github.com>
2026-08-09 02:54:43 +0900

    fix: migration 파일 오타 수정

--- src/main/resources/db/migration/V16__add_occurrence_date.sql
+++ src/main/resources/db/migration/V16__add_occurrence_date.sql
@@ -22,4 +22,4 @@ ALTER TABLE action_items
     ALTER COLUMN occurrence_date SET NOT NULL;
 
 CREATE INDEX idx_action_items_parent_event_occurrence
-    ON action_items(parent_event_id, occurrence_date);ㅎ
+    ON action_items(parent_event_id, occurrence_date);
